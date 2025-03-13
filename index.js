0cb9467b7bb316d7f71ba88c7a5781d2d80f759f
Ian <iankruze60@gmail.com>
2025-03-13 21:43:21 +0300

    adds modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    { year: "1998", result: "W"},
+    { year: "1997", result: "W"},
+    { year: "1989", result: "L"}
+  ];
+  
+  function superbowlWin(gameRecords) {
+    const win = gameRecords.find((game) => game.result === "W");
+    if (win) {
+      return win.year;
+    } else {
+      return undefined;
+    }
+  }
